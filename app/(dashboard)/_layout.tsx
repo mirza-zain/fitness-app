76daf0bcc561853c5588bc476ac6ad1a07a276ec
mirza-zain <mirzazain269@gmail.com>
2025-11-30 13:27:11 +0500

    API Issue

--- app/(dashboard)/_layout.tsx
+++ app/(dashboard)/_layout.tsx
@@ -1,32 +1,45 @@
-import { Stack, Tabs } from "expo-router"
-
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 const DashboardLayout = () => {
+
     return (
-        <>
-        <Tabs 
-            screenOptions={{ 
+        <Tabs
+            screenOptions={{
                 headerShown: false,
                 tabBarStyle: {
-                    backgroundColor: '#ffffff',
+                    backgroundColor: "#ffffff",
                     paddingTop: 10,
-                    height: 90
+                    height: 90,
                 },
-                tabBarActiveTintColor: "blue",
-                tabBarInactiveTintColor: "gray"
-             }}
+                tabBarActiveTintColor: "black",
+                tabBarInactiveTintColor: "gray",
+                tabBarLabelStyle: {
+                    fontSize: 12,
+                },
+            }}
         >
             <Tabs.Screen
                 name="home"
-                options={{ title: "Home" }} 
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Ionicons name={focused ? "home" : "home-outline"} size={size ?? 20} color={color} />
+                    ),
+                }}
             />
+
             <Tabs.Screen
                 name="about"
-                options={{ title: "About" }} 
+                options={{
+                    title: "About",
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Ionicons name={focused ? "person-circle" : "person-outline"} size={size ?? 20} color={color} />
+                    ),
+                }}
             />
         </Tabs>
-        </>
-    )
-}
+    );
+};
 
-export default DashboardLayout
+export default DashboardLayout;
--- app/(dashboard)/home.tsx
+++ app/(dashboard)/home.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
+// import React, { useEffect, useState } from 'react'
 import { Text, View } from 'react-native'
 
-const index = () => {
+const home = () => {
+  // interface Quotes {
+  //   _id: number,
+  //   sentence: string,
+  //   author: string
+  // }
+
+  // const [dataa, setData] = useState<Quotes | undefined>()
+  // // const urlQuote = 'https://api.gameofthronesquotes.xyz/v1/random'
+
+  // const dataFetch = async () => {
+  //   const response = await fetch(urlQuote);
+  //   const result = await response.json()
+  //   if(!result) {
+  //     throw new Error(`Status: 404`)
+  //   }
+  //   // console.log(result)
+  //   setData(result)
+  // }
+
+  // useEffect(() => { dataFetch() }, [])
+
   return (
-    <View className='w-full h-full flex '>
-      <Text>Hello</Text>
+    <View className="w-full h-full flex flex-1">
+      <Text className='text-2xl font-bold text-center font-["Fira"]' style={{marginTop: "20%", fontWeight: 'bold', padding: "5%"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci exercitationem fugit obcaecati.</Text>
+      <Text className='text-center font-semibold' style={{fontSize: 15}}>"Mirza Zain"</Text>
     </View>
   )
 }
 
-export default index
+export default home
