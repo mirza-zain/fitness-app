a6221ece5e479df92c31a4c949a9f59568e355ca
mirza-zain <mirzazain269@gmail.com>
2025-12-05 22:08:29 +0500

    Dashboard Upload

--- app/Components/Days.tsx
+++ app/Components/Days.tsx
@@ -1,71 +1,22 @@
-import { FlatList, ScrollView, Text, View } from "react-native"
+import { FlatList, Text, View } from "react-native"
+import weeks from "./DaysList"
 
 
 const Days = () => {
-    const weeks = [
-        {
-            id: 1,
-            day: "Mon",
-            items: [
-                {
-                    id: 1,
-                    name: "Push Ups",
-                    description: "exercise pushups",
-                    image: "path",
-                    isCompleted: false
-                },
-            ],
-        },
-        {
-            id: 2,
-            day: "Tues",
-            items: [
-                {
-                    id: 1,
-                    name: "Squats",
-                    description: "exercise squats",
-                    image: "path",
-                    isCompleted: false
-                },
-            ],
-        },
-        {
-            id: 3,
-            day: "Wed",
-            items: []
-        },
-                {
-            id: 4,
-            day: "Thurs",
-            items: []
-        },
-                {
-            id: 5,
-            day: "Fri",
-            items: []
-        },
-                {
-            id: 6,
-            day: "Sat",
-            items: []
-        },
-                {
-            id: 7,
-            day: "Sun",
-            items: []
-        },
-    ]
-
     return (
         <View className="w-full h-full p-5">
-            <Text className="text-2xl font-bold">Start Working Out!!!!</Text>
-            <View className="w-full flex flex-row">
-                {/* {
-                    weeks.map((items, index) => (
-                        
-                    ))
-                } */}
-            </View>
+           <FlatList 
+                data={weeks}
+                keyExtractor={(item) => String(item.id)}
+                contentContainerStyle= {{ padding: 5 }}
+                renderItem={({item}) => (
+                    <View>
+                        <View className="w-full flex flex-row justify-center">
+                            <Text>{item.day}</Text>
+                        </View>
+                    </View>
+                )}
+           />
         </View>
     )
 }
---
+++ app/Components/DaysList.tsx
@@ -0,0 +1,55 @@
+const Weeks = [
+        {
+            id: 1,
+            day: "Mon",
+            items: [
+                {
+                    id: 1,
+                    name: "Push Ups",
+                    description: "exercise pushups",
+                    image: "path",
+                    isCompleted: false
+                },
+            ],
+        },
+        {
+            id: 2,
+            day: "Tues",
+            items: [
+                {
+                    id: 1,
+                    name: "Squats",
+                    description: "exercise squats",
+                    image: "path",
+                    isCompleted: false
+                },
+            ],
+        },
+        {
+            id: 3,
+            day: "Wed",
+            items: []
+        },
+                {
+            id: 4,
+            day: "Thurs",
+            items: []
+        },
+                {
+            id: 5,
+            day: "Fri",
+            items: []
+        },
+                {
+            id: 6,
+            day: "Sat",
+            items: []
+        },
+                {
+            id: 7,
+            day: "Sun",
+            items: []
+        },
+    ]
+
+export default Weeks
